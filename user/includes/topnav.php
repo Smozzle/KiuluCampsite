ee5a9c0b789182f1339d5733ce70890f7facf1f0
smo <suzmosingkil@gmail.com>
2025-07-01 14:27:04 +0800

    Create topnav.php

---
+++ user/includes/topnav.php
@@ -0,0 +1,25 @@
+<!-- USER TOP NAV SECTION STARTS HERE -->
+<!--SYAMSUL ANIS NABILA BINTI SHAMSOL KAMAL (BI22110395)-->
+<nav>
+  <div class="nav__header">
+    <div class="nav__logo">
+      <a href="<?php echo BASE_URL; ?>/index.php" class="logo">KIULU CAMPSITE</a>
+    </div>
+    <div class="nav__menu__btn" id="menu-btn">
+      <i class="ri-menu-line"></i>
+    </div>
+  </div>
+  <ul class="nav__links" id="nav-links">
+    <li><a href="<?php echo BASE_URL; ?>/index.php">HOME</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/blog/blogList.php">BLOG</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/activity/activityList.php">ACTIVITIES</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/accommodation/accommodation.php">ACCOMMODATION</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/booking/bookingHistory.php">HISTORY</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/voucher/voucher.php">VOUCHER</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/FAQ/FAQ.php">FAQ</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/safety/safetyList.php">SAFETY TIPS</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/review/reviewForm.php">REVIEW</a></li>
+    <li><a href="#about">ABOUT</a></li>
+  </ul>
+</nav>
+<!-- USER TOP NAV SECTION ENDS HERE -->
